9a9e856a7a3e2d4851b9c4c5b4ba8eadfb426302
coderofphcity <agantemboy@gmail.com>
2024-01-10 10:48:04 +0100

    ff

--- ListOfNumbers/src/ListOfNumbers.sol
+++ ListOfNumbers/src/ListOfNumbers.sol
@@ -4,13 +4,32 @@ pragma solidity ^0.8.13;
 contract ListOfNumbers {
     uint256[] private arr;
 
+    event NumberAppended(address indexed caller, uint256 number);
+
+
     /// let caller append @param number to the array "arr"
     function appendToArray(uint256 number) public {
         // your code here
+        // address caller = msg.sender;
+        arr.push(number);
+        emit NumberAppended(msg.sender, number);
+    }
+    function deleteArray(uint index) public {
+         delete arr[index];
+
+    }
+    function removePop(uint index) public {
+        uint pop = arr[arr.length-1];
+        arr.pop();
+        arr[index] = pop;
+
+
     }
 
     /// return arr
-    function getArray() public returns (uint256[] memory) {
+    function getArray() public view returns (uint256[] memory) {
         // your code here
+        return arr;
+        
     }
 }
